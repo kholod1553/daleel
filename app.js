e2aa11b4b56a15065f7b51bc4950eef5589ca57f
khokha1553 <khokha1553@gmail.com>
2026-02-02 22:20:13 +0200

    export app

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 const supabaseUrl = 'https://rwfcmdmxirkidgtwttlg.supabase.co'; 
 const supabaseKey = 'sb_publishable_vbAjEVWkXWlS4pKyoMz0XQ_F_LsB_cl';
 const supabase = createClient(supabaseUrl, supabaseKey);
-module.exports = supabase;
+module.exports = supabase , app ;
 const locationRoutes = require('./routes/locationRoutes');
 app.use('/api/locations', locationRoutes);
 app.listen(port, () => {
