b059a7c833284f97dc1319b45dc14bbd2000b457
khokha1553 <khokha1553@gmail.com>
2026-02-02 22:05:27 +0200

    simplify vercel config

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { createClient } = require('@supabase/supabase-js');
 const cors = require('cors');
-
+require('dotenv').config();
 const app = express();
 const port = 3000;
 app.use(cors());
