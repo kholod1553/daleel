35b94b047a5e5e293cf1f19590b2031b078ee606
khokha1553 <khokha1553@gmail.com>
2026-02-03 11:08:34 +0200

    fix k

--- api/app.js
+++ api/app.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 const supabaseUrl = 'https://rwfcmdmxirkidgtwttlg.supabase.co'; 
-const supabaseKey = 'sb_publishable_vbAjEVWkXWlS4pKyoMz0XQ_F_LsB_cl';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ3ZmNtZG14aXJraWRndHd0dGxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5NDExMTcsImV4cCI6MjA4NTUxNzExN30.T7tImVbsvvpwtWVz--FJ1rlLQ3YMRREgAJYQfLalr8k';
 const supabase = createClient(supabaseUrl, supabaseKey);
 module.exports = supabase , app ;
 const locationRoutes = require('../routes/locationRoutes');
--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@ const { createClient } = require('@supabase/supabase-js');
 
 // استبدلي هذه القيم ببياناتك من Supabase Dashboard
 const supabaseUrl = 'https://rwfcmdmxirkidgtwttlg.supabase.co'; 
-const supabaseKey = 'sb_publishable_vbAjEVWkXWlS4pKyoMz0XQ_F_LsB_cl';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ3ZmNtZG14aXJraWRndHd0dGxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5NDExMTcsImV4cCI6MjA4NTUxNzExN30.T7tImVbsvvpwtWVz--FJ1rlLQ3YMRREgAJYQfLalr8k';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
