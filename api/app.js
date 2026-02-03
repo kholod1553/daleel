72ff99a2f71d78509bf851aaafd48d6d46d8edd8
khokha1553 <khokha1553@gmail.com>
2026-02-03 12:14:47 +0200

    clean up app.js and exports

--- api/app.js
+++ api/app.js
@@ -14,13 +14,10 @@ app.get('/favicon.ico', (req, res) => res.status(204).end());
 const supabaseUrl = 'https://rwfcmdmxirkidgtwttlg.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ3ZmNtZG14aXJraWRndHd0dGxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5NDExMTcsImV4cCI6MjA4NTUxNzExN30.T7tImVbsvvpwtWVz--FJ1rlLQ3YMRREgAJYQfLalr8k';
 const supabase = createClient(supabaseUrl, supabaseKey);
-app.get('/', (req, res) => {
-    res.send('✅ Daleel API is Running successfully!');
-});
 const locationRoutes = require('../routes/locationRoutes');
 app.use('/api/locations', locationRoutes);
 app.get('/', (req, res) => {
-    res.send('Daleel API is Running successfully! ✅');
+    res.send('Daleel API is Running successfully!');
 });
 module.exports = app;
 
